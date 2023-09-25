9abc310c1cbf9744ea4eccd3da838cedae0390eb
JorgeAz22 <ascencioluna92@gmail.com>
2023-09-25 08:37:57 -0600

    Practica 006
    dia 25 de septiembre en clase

---
+++ P006_Repaso_entrada y salida de datos/P006_Repaso_entrada y salida de datos.cpp
@@ -0,0 +1,44 @@
+// P006_Repaso_entrada y salida de datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int main()
+{
+    setlocale(LC_ALL, "es_MX.UTF-8");
+
+    std::cout << "Hola usuario dime tu edad para dejarte pasar!\n";
+    int mayor_edad = 18;
+    std::cin >> mayor_edad;
+    if (mayor_edad >=18)
+    std::cout << "Como eres mayor de edad puedes pasar\n ";
+
+    else
+    {
+        std::cout << "Eres menor de edad largate de aqui\n";
+    }
+    
+    std::cout << "Hola dime tu peso para poder dejarte subir a la atracion\n";
+    int peso = 60;
+    std::cin >> peso;
+        if (peso > 60)
+            std::cout << "Tienes el peso recomendable puedes pasar\n";
+
+        else
+        {
+            std::cout << "No tienes el  peso recomendable  no puedes subirte\n";
+        }
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
