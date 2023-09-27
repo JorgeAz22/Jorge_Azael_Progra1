14ece51fd9c62974acc1cdb318f6a0ebcd28dce8
JorgeAz22 <ascencioluna92@gmail.com>
2023-09-27 08:59:11 -0600

    Operadores Aritmeticos/Bibliotecas
    Practica

--- Practica_007_V00/Practica_007_V00.cpp
+++ Practica_007_V00/Practica_007_V00.cpp
@@ -10,53 +10,46 @@
 
 int main()
 {
-   
+	int num1, num2;
+	int operador;
 
-    float a, b, c;
-    a = 5;
-    b = 10;
-    c = a / b;
-    std::cout << "El resultado de la division es" << c << std::endl;
+	std::cout << "Ingrese el primer numero por favot\n";
+	std::cin >> num1;
 
-    float d, e, f;
-    d = 5;
-    e = 2;
-    f = d - e;
-    std::cout << "El resultado de la resta es" << f << std::endl;
+	std::cout << "Ingrese un operador (1.- +, 2.- -, 3.- *,4- /,5- ^,):\n";
+	std::cin >> operador;
 
-    float g, h, i;
-    g = 5;
-    h = 23;
-    i = g + h;
-    std::cout << "El resultado de la suma es" << c << std::endl;
+	std::cout << "Ingrese el segundo numero\n";
+	std::cin >> num2;
 
-    float j, k, l;
-    j = 3;
-    k = a;
-    l = j * k;
-    std::cout << "El resultado de la multipiclacion es" << l << std::endl;
+	int resultado;
+	switch (operador)
+	{
 
-    float m, n;
-    m = 3;
-    n = sqrt(3);
+	case 1:
+		resultado = num1 + num2;
 
-    std::cout << "El resultado de la raiz es" << n << std::endl;
+		std::cout << "El resultado de la suma es " << resultado << std::endl;
+	break;
+	case 2:
+			resultado = num1 - num2;
+			std::cout << "El resultado de la resta es " << resultado << std::endl;
+			break;
+	case 3:
+			resultado = num1 * num2;
+			std::cout << "El resultado de la multiplacion es " << resultado << std::endl;
+			break;
+	case 4:
+			resultado = num1 / num2;
+			std::cout << "El resultado de la division es " << resultado << std::endl;
+			break;
+	case 5:
+			resultado = num1 ^ num2;
+			std::cout << "El resultado de la potencia es " << resultado << std::endl;
+			break;
+      default:
+		  std::cout << "Operador invalido\n";
+		  return 1;
+	}
 
-    float o, p;
-    o = 3;
-    p = pow(3, 2);
-    std::cout << "El resultado de la potencia es" << p << std::endl;
-
-   
-
-}
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	}
